35cf1b3496d1b971e836628beac3a149ca6566c7
Ginna Mora <higinnamora@gmail.com>
2021-03-05 15:35:35 -0500

    Hiwaves-Frequency: add localStorage: dark-mode

--- js/main.js
+++ js/main.js
@@ -3,4 +3,20 @@ const btnSwith = document.querySelector('#switch');
 btnSwith.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   btnSwith.classList.toggle('active');
-})
+
+  // Guardamos el modo en localstorage
+  if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+// Obtenemos el modo actual
+if(localStorage.getItem('dark-mode') === 'true'){
+  document.body.classList.add('dark');
+  btnSwith.classList.add('active');
+} else {
+  document.body.classList.remove('dark');
+  btnSwith.classList.remove('active');
+}
